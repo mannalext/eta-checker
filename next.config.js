79b9cb7c3804b54041ae1c046645983857ab9a83
Alex Mann <amann@physna.com>
2023-07-26 14:39:13 -0400

    fix cors issue occurring on main site

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   basePath: '/eta-checker',
+  async headers() {
+    return [
+      {
+        // Match the route of your API route
+        source: '/eta-checker/api/directions',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'alexmann.dev',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,DELETE,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
